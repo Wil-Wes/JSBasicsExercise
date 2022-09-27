262da46fa70ab2c93c9ec46853a197f066e0f68a
Wil-Wes <will.d.wes@gmail.com>
2022-09-27 09:38:25 -0500

    JSBasicsExerciseComplete

---
+++ script.js
@@ -0,0 +1,67 @@
+//1
+console.log(3);
+
+//2
+/*
+Number
+String
+Boolean
+Null
+Undefined
+*/
+
+//3
+/*
+Addition is *
+Subtract is -
+Multiplication *
+Division is /
+Modulus is %
+Exponentiation is **
+
+Future self:
+increment ++
+decrement --
+*/
+
+//4
+console.log(5 % 2);//1
+console.log(5 % 4);//1
+
+//5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+//6
+console.log("Hawk" + "eye");
+
+//7
+console.log("William".length + "Wesley".length);
+
+//8
+console.log("Avengers: Endgame"[16]);
+
+//BONUS
+//1
+console.log("\/");
+
+//2
+console.log(5 ** 4 - 9);
+
+//3
+console.log(("I".length + "Am".length + "Iron".length + "Man".length) * 0);
+
+//4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+//5
+let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u);
